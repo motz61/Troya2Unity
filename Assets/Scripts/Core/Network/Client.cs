0b64daeb3b16afcc928f2cdb9d9df641f4dc3e48
Moţ <iustin.danut@gmail.com>
2022-02-13 09:30:37 +0200

    2D empty scene.

---
+++ Assets/Scripts/Core/Network/Client.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Network
+{
+    public class Client : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        private void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        private void Update()
+        {
+
+        }
+    }
+}
